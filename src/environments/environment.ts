72248fb267414e9d56e3aac494d794bdad75a5b8
Jose-Marcello <josemarcellogardeldealemar@gmail.com>
2025-12-24 15:42:37 -0300

    Corrigindo environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
  // URL do backend que está no ACA do Azured:
-  //apiUrl:'https://api-bancodeitens-qa.thankfultree-b9485fed.brazilsouth.azurecontainerapps.io',
+  apiUrl:'https://api-bancodeitens-qa.thankfultree-b9485fed.brazilsouth.azurecontainerapps.io',
   // URL do backend que está no Railway:
   //apiUrl: 'https://bancodeitens-v4-production.up.railway.app' 
 
   //apiUrl: '/',
-  apiUrl: 'https://localhost:5001',
+  //apiUrl: 'https://localhost:5001',
   imagesUrl: '/assets/images/',
 
  // 🔑 FLAG CRÍTICA: Ativa o MOCK para reenvio de e-mail no desenvolvimento
