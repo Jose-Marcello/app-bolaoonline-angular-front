eefaacc8647e9568902d708fe6462658a1b03020
Jose-Marcello <josemarcellogardeldealemar@gmail.com>
2026-01-05 10:27:01 -0300

    Acerto environmet stage

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // URL do backend que está no ACA do Azured:
   //apiUrl:'https://env-bolao-qa.gentlerock-116715ed.brazilsouth.azurecontainerapps.io',
-  apiUrl:'/',
+  apiUrl:'',
   //apiUrl: 'https://localhost:5001',
  
  imagesUrl: '/assets/images/',
