e74efebcc2efc26a4333cdd82cfc10e620ccc106
Jose-Marcello <josemarcellogardeldealemar@gmail.com>
2026-01-25 13:40:19 -0300

    Ajuste no ENVIRONMENTS para PRODUÇÃO (STAGING)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   // URL do backend que está no ACA do Azured:
- //apiUrl:'https://aca-bolao-backend.bluebay-3fff5721.brazilsouth.azurecontainerapps.io',
+ apiUrl:'https://aca-bolao-backend.bluebay-3fff5721.brazilsouth.azurecontainerapps.io',
   //apiUrl: 'http://localhost:8080',   //execução local
-  apiUrl: '',
+ // apiUrl: '',
 
   imagesUrl: '/assets/images/',
  isMockEnabled: true
