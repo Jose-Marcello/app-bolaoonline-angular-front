9191d3e59238f40a9f7a7d195c438883a12a9e5b
Jose-Marcello <josemarcellogardeldealemar@gmail.com>
2026-01-25 12:27:07 -0300

    Ajuste dos ENVIRONMENST para BANDO DE STAGING/PROD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   // URL do backend que está no ACA do Azured:
  //apiUrl:'https://aca-bolao-backend.bluebay-3fff5721.brazilsouth.azurecontainerapps.io',
-  apiUrl: 'http://localhost:8080',   //execução local
+  //apiUrl: 'http://localhost:8080',   //execução local
+  apiUrl: '',
 
   imagesUrl: '/assets/images/',
  isMockEnabled: true
